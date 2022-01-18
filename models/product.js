c41b1d4cfb8d22927a4cdead39ac45d1a7b71627
AghansCode <ganis.ade23@gmail.com>
2022-01-18 21:40:57 +0700

    update view node mvc

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const { timeStamp } = require("console");
 const fs = require("fs");
 const path = require("path");
 
@@ -14,8 +15,11 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
   save() {
     getProductsFromFile((products) => {
